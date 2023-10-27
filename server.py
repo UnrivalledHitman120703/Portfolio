b3b1701f1f5edee079cf8cd222dd293aee00e8fb
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-27 20:43:01 +0530

    changed file path in index.html and server.py

--- server.py
+++ server.py
@@ -9,23 +9,8 @@ app = Flask(__name__)
 
 # A normal routing
 @app.route("/")
-def index():
+def my_():
     return render_template("index.html")
 
 
-# Url parameters
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template("name.html", name=username, post_id=post_id)
 
-
-# A normal routing
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-# Creating a blog routing
-@app.route("/blog")
-def hello_bloggers():
-    return "<h1><center>Hello Bloggers!!</center></h1"
