5da2375a4bcef75894f76fd0700b865d07fd0c0a
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-27 20:55:33 +0530

    Fixed some bugs and added functionality

--- server.py
+++ server.py
@@ -7,10 +7,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# A normal routing
-@app.route("/")
-def my_():
+# index.html routing
+@app.route("/index.html")
+def home():
     return render_template("index.html")
 
 
+# about.html routing
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
 
+
+# work.html routing
+@app.route("/works.html")
+def work():
+    return render_template("works.html")
+
+
+# contact.html routing
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
